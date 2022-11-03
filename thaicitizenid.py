5b11813bb01cd37e20039fe5ca1ee5643a6a0404
manatsarin <51470581+manatsarin@users.noreply.github.com>
2022-11-03 09:18:01 +0700

    Update thaicitizenid.py

--- thaicitizenid.py
+++ thaicitizenid.py
@@ -3,15 +3,20 @@
 
 def validate(id):
   if len(id) != 13:
-    print("Invalid id size!")
+    return False
   else:
     sum = 0
     for i in range(12):
       sum = sum + int(id[i])*(13-i)
-      print(id[i], i, 13-i, int(id[i])*(13-i), sum)
+      #print(id[i], i, 13-i, int(id[i])*(13-i), sum)
     if ((11 - (sum % 11)) % 10) != int(id[12]):
-      print("Invalid check sum!", (sum % 11))
-    print("valid")
+      #print("Invalid check sum!", (sum % 11))
+      return False
+    return True
 
 
-validate("4928347404300")
+if validate("4928347404300"):
+   print('OK')
+else:
+   print('Incorrect')
+
